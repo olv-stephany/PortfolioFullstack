2d3d2f53d48f3aa232b34c7fa4ca56dd6eb9a9d4
Stephany Oliveira da Silva <stephany16ce@outlook.com>
2025-07-28 13:41:37 -0300

    feat: first section

---
+++ src/pages/LandingPage.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+
+const LandingPage = () => {
+  return (
+    <div>
+        <section className="welcome-container">
+            <h2>Prazer, eu sou a <span><h1>Stephany</h1></span></h2>
+            <ul className='navegation-container'>
+                <li>Habilidades</li>
+                <li>Sobre</li>
+                <li>Projeto</li>
+                <li>Contato</li>
+            </ul>
+        </section>
+    </div>
+  )
+}
+
+export default LandingPage
