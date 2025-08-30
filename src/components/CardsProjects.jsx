490533eb122bbecbc9f8e00c9a6f369565e7513a
Stephany Oliveira da Silva <stephany16ce@outlook.com>
2025-08-30 17:05:10 -0300

    feat: added style+responsitivity on the page

--- src/components/CardsProjects.jsx
+++ src/components/CardsProjects.jsx
@@ -1,6 +1,6 @@
 import React, { useState } from "react";
 import { Link } from "react-router-dom";
-import vibraniumImage from "../images/projects/v-api.jpg";
+import vibraniumImage from "../images/projects/v-api/v-api.jpg";
 import { FiChevronRight } from "react-icons/fi";
 
 const projects = [
